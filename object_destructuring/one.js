fc58efc7aed92b0190fcc6948154cbf1e17d0ae6
sravyasravs008 <106951281+sravyasravs008@users.noreply.github.com>
2022-08-04 19:48:13 +0530

    ok

---
+++ object_destructuring/one.js
@@ -0,0 +1,11 @@
+let emp={
+    id:202,name:"rahul",sal:40000,
+ details:{email:"rahul@tcs.com",login_id:40404},
+ loc:["banglore","goa"]
+}
+console.log(emp.id)
+console.log(emp.details.email)
+
+let{name,loc}=emp
+console.log(name)
+console.log(loc)
---
+++ object_destructuring/two.js
@@ -0,0 +1,9 @@
+let product={
+    pro:"samsung",model:'flip',price:70000,
+    loc:["banglore","delhi","kerala"],
+    colour:["black","white","grey"]
+}
+console.log(product.pro)
+let{pro,colour}=product
+console.log(pro)
+console.log(colour)
